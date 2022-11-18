063de342410b6694038e4b1942d0b429b8275567
legrism <legrism@educ.college-montreal.qc.ca>
2022-11-18 15:35:36 -0500

    commit initial

--- main.py
+++ main.py
@@ -3,21 +3,25 @@ import random
 
 boucle = True
 while boucle:
-    chiffre = random.randint (1,10)
-
+    borne_minimale = int(input('choisissez un nombre minimal'))
+    borne_maximale = int(input('choisissez un nombre maximal.'))
+    chiffre = random.randint(borne_minimale, borne_maximale)
 
     boucle_deviner = True
+    compteur_essai = 0
     while boucle_deviner:
-        nombre = int(input('essayer de deviner  un chiffre de 1 a 10.'))
+
+        nombre = int(input('essayer de deviner  un chiffre aleatoire entre votre nombre minimal et maximal.'))
         if nombre < chiffre:
             print('Votre chiffre est trop bas, essayez plus haut.')
-
         elif nombre > chiffre:
             print('Le chiffre que vous avez deviné est trop haut.')
-
         else:
             print('Vous avez trouvé le bon chiffre, felicitation.')
+            print("vous avez réussis en " + str(compteur_essai) + " essais" )
             boucle_deviner= False
+
+        compteur_essai += 1
 
     rejouer = input("Tu as gagné veux-tu rejouer (oui/non)?")
 
